e4ab174b0ba9a28334add4afd700f864e78b8cc3
ligun <ligun.net@gmail.com>
2013-07-28 01:45:05 +0900

    シリアル通信制御追加

--- StalCar_Arduino.ino
+++ StalCar_Arduino.ino
@@ -1,47 +1,106 @@
-#define LEFT_MOTER1 1
-#define LEFT_MOTER2 2
-#define RIGHT_MOTER1 3
-#define RIGHT_MOTER2 4
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <MotorDriver.h>
+#include "UltraSonicSensor.h"
+
+#define LEFT_MOTOR1 2
+#define LEFT_MOTOR2 3
+#define RIGHT_MOTOR1 4
+#define RIGHT_MOTOR2 5
+
+//MotorDriver *left,*right;
+MotorDriver left(LEFT_MOTOR1,LEFT_MOTOR2),right(RIGHT_MOTOR1,RIGHT_MOTOR2);
 
 void setup(){
-  pinMode(LEFT_MOTER1,OUTPUT);
-  pinMode(LEFT_MOTER2,OUTPUT);
-  pinMode(RIGHT_MOTER1,OUTPUT);
-  pinMode(RIGHT_MOTER2,OUTPUT);
+  pinMode(8,INPUT);
   
-  randomSeed(analogRead(0));
+  setupUltraSonicSensor(14,15);
+  Serial.begin(9600);
 }
 
 void loop(){
-  int hoge = random(3);
-  switch(hoge){
-    case 0:
-    digitalWrite(LEFT_MOTER1,HIGH);
-    digitalWrite(LEFT_MOTER2,LOW);
-    digitalWrite(RIGHT_MOTER1,HIGH);
-    digitalWrite(RIGHT_MOTER2,LOW);
-    break;
-    case 1:
-    digitalWrite(LEFT_MOTER1,HIGH);
-    digitalWrite(LEFT_MOTER2,LOW);
-    digitalWrite(RIGHT_MOTER1,LOW);
-    digitalWrite(RIGHT_MOTER2,LOW);
-    break;
-    case 2:
-    digitalWrite(LEFT_MOTER1,LOW);
-    digitalWrite(LEFT_MOTER2,LOW);
-    digitalWrite(RIGHT_MOTER1,HIGH);
-    digitalWrite(RIGHT_MOTER2,LOW);
-    break;
-    /*
-    case 3:
-    digitalWrite(LEFT_MOTER1,LOW);
-    digitalWrite(LEFT_MOTER2,LOW);
-    digitalWrite(RIGHT_MOTER1,LOW);
-    digitalWrite(RIGHT_MOTER2,LOW);
-    break;
-    */
+  char buf[64];
+  read(buf,64);
+  Serial.println(buf);
+  parse(buf);
+  /*
+  if(digitalRead(8)){
+    mode1();
+    Serial.println("mode1");
   }
-  delay(5000);
+  else mode2();
+  */
 }
 
+void mode1(){
+  float distance = measureDistance(14,15); 
+  if(distance > 50){
+    left.forward();
+    right.forward();
+  }else{
+    left.forward();
+    right.stop();
+  }
+  Serial.println(distance);
+  delay(1);
+}
+
+void mode2(){
+  float distance = measureDistance(14,15);
+  if(distance > 30){
+    left.forward();
+    right.forward();
+  }else if(distance >20){
+    left.stop();
+    right.stop();
+  }else{
+    left.brake();
+    right.brake();
+  }
+  Serial.println(distance);
+}
+
+int read(char *buf,int size){
+  int length,i = 0;
+  while(1){
+    if(Serial.available()){
+        buf[i] = Serial.read();
+        if(buf[i] == '\n')break;
+        ++i;
+    }
+  }
+  buf[i] = '\0';
+  return 0;
+}
+
+int parse(char *buf){
+  char *motor,*muki,*speed_s;
+  int speed;
+  motor = strtok(buf,",");
+  muki = strtok(NULL,",");
+  speed_s = strtok(NULL,",");
+  speed = atoi(speed_s);
+  
+  Serial.print("motor:");
+  Serial.println(motor);
+  Serial.print("muki:");
+  Serial.println(muki);
+  Serial.print("speed:");
+  Serial.println(speed);
+  
+  if(strcmp(motor,"left") == 0){
+    if(strcmp(muki,"forward") == 0)left.forward(speed);
+    if(strcmp(muki,"reverse") == 0)left.reverse(speed);
+    if(strcmp(muki,"stop") == 0)left.stop();
+    if(strcmp(muki,"brake") == 0)left.brake();
+  }
+  else if(strcmp(motor,"right") == 0){
+    if(strcmp(muki,"forward") == 0)right.forward(speed);
+    if(strcmp(muki,"reverse") == 0)right.reverse(speed);
+    if(strcmp(muki,"stop") == 0)right.stop();
+    if(strcmp(muki,"brake") == 0)right.brake();
+  }
+  
+  return 0;
+}
